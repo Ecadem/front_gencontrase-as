c60f70eec4aacedb1d574f9b17ca155b746bdef5
Eduardo <eduardovalenzuelaparga@gmail.com>
2023-03-12 20:53:17 -0500

    Revert "se retiran archivos"
    This reverts commit 95a48379ed1743c813ac09e41b60599c74ec18ef.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,26 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  items: MenuItem[] = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Contacto',
+        icon: 'pi pi-linkedin',
+        url: 'https://www.linkedin.com/in/eduardovparga/'
+      },
+      {
+        label: 'Pagina web',
+        icon: 'pi pi-globe',
+        url: 'https://ecadem.co/'
+      },
+      {
+        label: 'Repos',
+        icon: 'pi pi-github',
+        url: 'https://github.com/Ecadem'
+      }
+    ];
+  }
 
 }
