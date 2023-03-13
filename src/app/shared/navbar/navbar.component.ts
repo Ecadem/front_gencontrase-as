95a48379ed1743c813ac09e41b60599c74ec18ef
Eduardo <eduardovalenzuelaparga@gmail.com>
2023-03-12 20:49:05 -0500

    se retiran archivos

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -8,26 +7,7 @@ import {MenuItem} from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
 
-  items: MenuItem[] = [];
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Contacto',
-        icon: 'pi pi-linkedin',
-        url: 'https://www.linkedin.com/in/eduardovparga/'
-      },
-      {
-        label: 'Pagina web',
-        icon: 'pi pi-globe',
-        url: 'https://ecadem.co/'
-      },
-      {
-        label: 'Repos',
-        icon: 'pi pi-github',
-        url: 'https://github.com/Ecadem'
-      }
-    ];
-  }
+  ngOnInit() {}
 
 }
